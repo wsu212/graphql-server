e2ae156fa2c0c39c5c2b128407f33a02371cf081
Wei-Lun Su <48189264+wsu212@users.noreply.github.com>
2024-09-05 08:43:25 -0400

    add dependencies

--- Package.swift
+++ Package.swift
@@ -15,6 +15,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        
+        .package(url: "https://github.com/alexsteinerde/graphql-kit.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -25,6 +27,7 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "GraphQLKit", package: "graphql-kit"),
             ],
             swiftSettings: swiftSettings
         ),
