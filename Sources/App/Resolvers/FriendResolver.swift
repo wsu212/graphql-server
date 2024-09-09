ce3746fb8bc362acd00b0d6039581c322ac6328a
Wei-Lun Su <48189264+wsu212@users.noreply.github.com>
2024-09-09 19:46:26 -0400

    update list of friends

--- Sources/App/Resolvers/FriendResolver.swift
+++ Sources/App/Resolvers/FriendResolver.swift
@@ -11,12 +11,20 @@ import GraphQLKit
 
 // Post Resolver to handle fetching friends
 struct FriendResolver {
-    private let posts: [Friend] = [
-        Friend(regiId: 1, name: "First Friend"),
-        Friend(regiId: 2, name: "Second Friend")
+    private let friends: [Friend] = [
+        Friend(regiId: 1, name: "Francisco Lindor"),
+        Friend(regiId: 2, name: "Brandon Nimmo"),
+        Friend(regiId: 3, name: "Jeff McNeil"),
+        Friend(regiId: 4, name: "Kodai Senga"),
+        Friend(regiId: 5, name: "Edwin Díaz"),
+        Friend(regiId: 6, name: "Fernando Tatis Jr."),
+        Friend(regiId: 7, name: "Yu Darvish"),
+        Friend(regiId: 8, name: "Blake Snell"),
+        Friend(regiId: 9, name: "Freddie Freeman"),
+        Friend(regiId: 10, name: "Mookie Betts"),
     ]
     
     func getAllFriends(request: Request, _: NoArguments) throws -> [Friend] {
-        posts
+        friends
     }
 }
